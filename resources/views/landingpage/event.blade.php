5a10e1bb63e46149a4770e82c76395bbca813f94
sesilliayam <sesilliafajar@gmail.com>
2019-12-12 20:03:09 +0700

    fixing event slider

--- resources/views/landingpage/event.blade.php
+++ resources/views/landingpage/event.blade.php
@@ -4,136 +4,90 @@
       <h2 class="text-center mt-0">Event Terdekat</h2>
       <hr class="divider my-4">
       <div class="row">
-          <div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel"  data-interval="1000">
+          <div class="MultiCarousel" data-items="1,3,5,3" data-slide="1" id="MultiCarousel"  data-interval="1000">
             <div class="MultiCarousel-inner">
                 <div class="item">
                     <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
+                        <p class="lead"><b>Gotong Royong Perbaikan Bukit</b></p>
+                        <img src="{{asset('img/event/e_1.jpg')}}" alt="" class="kotak">
+                        <br><br>
+                        <p>Memperbaiki bukit gersang</p>
+                        <p><i class="fas fa-map-marker-alt fa-1x text-primary"></i> Sukabumi | <i class="fas fa-calendar-alt fa-1x text-primary"></i> Sukabumi</p>
                     </div>
                 </div>
                 <div class="item">
                     <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
+                        <p class="lead"><b>Gotong Royong Perbaikan Bukit</b></p>
+                        <img src="{{asset('img/event/e_1.jpg')}}" alt="" class="kotak">
+                        <br><br>
+                        <p>Memperbaiki bukit gersang</p>
+                        <p><i class="fas fa-map-marker-alt fa-1x text-primary"></i> Sukabumi | <i class="fas fa-calendar-alt fa-1x text-primary"></i> Sukabumi</p>
                     </div>
                 </div>
                 <div class="item">
                     <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
+                        <p class="lead"><b>Gotong Royong Perbaikan Bukit</b></p>
+                        <img src="{{asset('img/event/e_1.jpg')}}" alt="" class="kotak">
+                        <br><br>
+                        <p>Memperbaiki bukit gersang</p>
+                        <p><i class="fas fa-map-marker-alt fa-1x text-primary"></i> Sukabumi | <i class="fas fa-calendar-alt fa-1x text-primary"></i> Sukabumi</p>
                     </div>
                 </div>
                 <div class="item">
                     <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
+                        <p class="lead"><b>Gotong Royong Perbaikan Bukit</b></p>
+                        <img src="{{asset('img/event/e_1.jpg')}}" alt="" class="kotak">
+                        <br><br>
+                        <p>Memperbaiki bukit gersang</p>
+                        <p><i class="fas fa-map-marker-alt fa-1x text-primary"></i> Sukabumi | <i class="fas fa-calendar-alt fa-1x text-primary"></i> Sukabumi</p>
                     </div>
                 </div>
                 <div class="item">
                     <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
+                        <p class="lead"><b>Gotong Royong Perbaikan Bukit</b></p>
+                        <img src="{{asset('img/event/e_1.jpg')}}" alt="" class="kotak">
+                        <br><br>
+                        <p>Memperbaiki bukit gersang</p>
+                        <p><i class="fas fa-map-marker-alt fa-1x text-primary"></i> Sukabumi | <i class="fas fa-calendar-alt fa-1x text-primary"></i> Sukabumi</p>
                     </div>
                 </div>
                 <div class="item">
                     <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
+                        <p class="lead"><b>Gotong Royong Perbaikan Bukit</b></p>
+                        <img src="{{asset('img/event/e_1.jpg')}}" alt="" class="kotak">
+                        <br><br>
+                        <p>Memperbaiki bukit gersang</p>
+                        <p><i class="fas fa-map-marker-alt fa-1x text-primary"></i> Sukabumi | <i class="fas fa-calendar-alt fa-1x text-primary"></i> Sukabumi</p>
                     </div>
                 </div>
                 <div class="item">
                     <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
+                        <p class="lead"><b>Gotong Royong Perbaikan Bukit</b></p>
+                        <img src="{{asset('img/event/e_1.jpg')}}" alt="" class="kotak">
+                        <br><br>
+                        <p>Memperbaiki bukit gersang</p>
+                        <p><i class="fas fa-map-marker-alt fa-1x text-primary"></i> Sukabumi | <i class="fas fa-calendar-alt fa-1x text-primary"></i> Sukabumi</p>
                     </div>
                 </div>
                 <div class="item">
                     <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
+                        <p class="lead"><b>Gotong Royong Perbaikan Bukit</b></p>
+                        <img src="{{asset('img/event/e_1.jpg')}}" alt="" class="kotak">
+                        <br><br>
+                        <p>Memperbaiki bukit gersang</p>
+                        <p><i class="fas fa-map-marker-alt fa-1x text-primary"></i> Sukabumi | <i class="fas fa-calendar-alt fa-1x text-primary"></i> Sukabumi</p>
                     </div>
                 </div>
                 <div class="item">
                     <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
+                        <p class="lead"><b>Gotong Royong Perbaikan Bukit</b></p>
+                        <img src="{{asset('img/event/e_1.jpg')}}" alt="" class="kotak">
+                        <br><br>
+                        <p>Memperbaiki bukit gersang</p>
+                        <p><i class="fas fa-map-marker-alt fa-1x text-primary"></i> Sukabumi | <i class="fas fa-calendar-alt fa-1x text-primary"></i> Sukabumi</p>
                     </div>
                 </div>
-                <div class="item">
-                    <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
-                    </div>
-                </div>
+                
             </div>
             <button class="btn btn-primary leftLst"><</button>
             <button class="btn btn-primary rightLst">></button>
